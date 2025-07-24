79fd22fb44dc5ef6fc0006bf698edaa3885ee642
kimhyera <khr1828@naver.com>
2025-07-24 12:00:11 +0900

    fix: 애니메이션 추가

--- contents/com/datas/datas.ebook.js
+++ contents/com/datas/datas.ebook.js
@@ -1,11 +1,96 @@
 if (typeof DATAS == 'undefined') DATAS = {};
 DATAS.ebook = {};
-DATAS.ebook.page = [
-    [0, 241]
-];
+DATAS.ebook.page = [[0, 241]];
 DATAS.ebook.size = {
-    width: 845,
-    height: 1080
+  width: 845,
+  height: 1080
 };
 DATAS.ebook.events = {
+  page_10: [
+    {
+      idx: '1',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '707',
+      y: '910',
+      icon: 'turn/icons/예시 답안.png',
+      tip: '예시 답안',
+      popup: '1-1-1',
+      group: '13-2'
+    }
+  ],
+
+  page_13: [
+    {
+      idx: '1',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '262',
+      y: '209',
+      icon: 'turn/icons/동영상.png',
+      tip: '동영상',
+      popup: '1-1-1',
+      group: '13-1'
+    },
+    {
+      idx: '5',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '707',
+      y: '910',
+      icon: 'turn/icons/예시 답안.png',
+      tip: '예시 답안',
+      popup: '1-1-1',
+      group: '13-2'
+    }
+  ],
+	
+
+  page_34: [
+    {
+      idx: '1',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '262',
+      y: '209',
+      icon: 'turn/icons/예시 답안.png',
+      tip: '예시 답안',
+      popup: '1-1-4',
+      group: '34-1'
+    }
+	],
+	
+
+  page_48: [
+    {
+      idx: '1',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '262',
+      y: '209',
+      icon: 'turn/icons/예시 답안.png',
+      tip: '예시 답안',
+      popup: '1-2-3',
+      group: '48-2'
+    }
+	],
+	  page_16: [
+    {
+      idx: '1',
+      type: 'popup',
+      w: '31',
+      h: '31',
+      x: '262',
+      y: '209',
+      icon: 'turn/icons/예시 답안.png',
+      tip: '예시 답안',
+      popup: '1-1-1',
+      group: '16-1'
+    }
+	]
 };
